3ffc2891e9c2384ef211ea7cd16c962db053a288
juan <juanxhoo@gmail.com>
2024-01-13 17:19:33 -0500

    adding Reports Actions Pages and Cricuits Layout

--- src/layouts/reports/CreateReport.js
+++ src/layouts/reports/CreateReport.js
@@ -1,30 +1,147 @@
 import * as React from 'react';
 
 import Container from '@mui/material/Container';
-import { Box, Paper } from '@mui/material';
-
+import { Paper, Grid, Typography, Stack, TextField, Button } from '@mui/material';
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
+import { useState } from 'react';
 // TODO remove, this demo shouldn't need to reset the theme.
+import dayjs from 'dayjs';
+import { DemoContainer, DemoItem } from '@mui/x-date-pickers/internals/demo';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import { MobileDatePicker } from '@mui/x-date-pickers/MobileDatePicker';
+import { DesktopDatePicker } from '@mui/x-date-pickers/DesktopDatePicker';
+import { StaticDatePicker } from '@mui/x-date-pickers/StaticDatePicker';
+import DropFile from '../components/DropFile';
+
 
 export default function CreateReport() {
 
+    const [value, setValue] = useState('');
+
     return (
-        <Container component="main">
-            <Box
+        <Grid container>
+            <Grid item xs={6}
                 sx={{
-                    mt: 8,
-                    display: 'flex',
-                    flexDirection: 'column',
-                    alignItems: 'center',
+                    mt: 4,
                     mb: 8
                 }}
             >
                 <Paper elevation={5} sx={{ p: 5 }}>
+                    <ReactQuill theme="snow" value={value} onChange={setValue} />
+                    <Stack direction="column"
+                        sx={{
+                            padding: '30px',
+                            marginTop: 8,
+                        }} component="form">
+                        <Grid container spacing={2}>
+                            <Grid item xs={12}>
+                                <Typography sx={{ fontWeight: 'bold' }}>Apellidos</Typography>
+                                <TextField
+                                    fullWidth
+                                    sx={{ minWidth: "400px" }}
+                                    margin="normal"
+                                    required
+                                    name="email"
+                                    autoComplete="email"
+                                    autoFocusrequired
+                                    id="outlined-required"
+                                    label="Required"
+                                    defaultValue="Hello World"
+                                />
+                            </Grid>
 
-                    Create
+                            <Grid item xs={12}>
+                                <Typography sx={{ fontWeight: 'bold' }}>correo</Typography>
+                                <TextField
+                                    fullWidth
+                                    margin="normal"
+                                    required
+                                    name="email"
+                                    autoComplete="email"
+                                    autoFocusrequired
+                                    id="outlined-required"
+                                    label="Required"
+                                    defaultValue="Hello World"
+                                />
+                            </Grid>
+
+
+                            <Grid item xs={12}>
+                                <Typography sx={{ fontWeight: 'bold' }}>Telefono</Typography>
+                                <TextField
+                                    fullWidth
+                                    margin="normal"
+                                    required
+                                    name="email"
+                                    autoComplete="email"
+                                    autoFocusrequired
+                                    id="outlined-required"
+                                    label="Required"
+                                    defaultValue="Hello World"
+                                />
+                            </Grid>
+
+
+                            <Grid item xs={12} >
+                                <Typography sx={{ fontWeight: 'bold' }}>Tipo de Sangre:</Typography>
+                                <TextField
+                                    fullWidth
+                                    margin="normal"
+                                    required
+                                    name="email"
+                                    autoComplete="email"
+                                    autoFocusrequired
+                                    id="outlined-required"
+                                    label="Required"
+                                    defaultValue="Hello World"
+                                />
+                            </Grid>
+                        </Grid>
+                    </Stack>
+
                 </Paper>
+            </Grid>
+            <Grid item
+                xs={6}
+                sx={{
+                    mt: 8,
+                    mb: 8
+                }}
+            >
+                <Paper elevation={5} sx={{ p: 5 }}>
+                    <LocalizationProvider dateAdapter={AdapterDayjs}>
+                        <DemoContainer
+                            components={[
+                                'DatePicker',
+                                'MobileDatePicker',
+                                'DesktopDatePicker',
+                                'StaticDatePicker',
+                            ]}
+                        >
+                            <DemoItem label="Desktop variant">
+                                <DesktopDatePicker defaultValue={dayjs('2022-04-17')} />
+                            </DemoItem>
+                            <DemoItem label="Mobile variant">
+                                <MobileDatePicker defaultValue={dayjs('2022-04-17')} />
+                            </DemoItem>
+                            <DemoItem label="Responsive variant">
+                                <DatePicker defaultValue={dayjs('2022-04-17')} />
+                            </DemoItem>
+                            <DemoItem label="Static variant">
+                                <StaticDatePicker defaultValue={dayjs('2022-04-17')} />
+                            </DemoItem>
+                        </DemoContainer>
+                    </LocalizationProvider>
+                    <DropFile/>
+                </Paper>
+                <Button variant="contained" size="large">Generar Reporte</Button>
+            </Grid>
 
 
-            </Box>
-        </Container>
+        </Grid>
+
     );
 }
--- src/layouts/reports/Report.js
+++ src/layouts/reports/Report.js
@@ -1,30 +1,129 @@
 import * as React from 'react';
 
 import Container from '@mui/material/Container';
-import { Box, Paper } from '@mui/material';
-
+import { Box, Paper, Grid, Typography, CardMedia, CardContent, Button, CardActions, Card, TextField, Stack } from '@mui/material';
+import ReactQuill from 'react-quill';
+import { useState } from 'react';
+import CloudDownloadIcon from '@mui/icons-material/CloudDownload';
+import PictureAsPdfIcon from '@mui/icons-material/PictureAsPdf';
 // TODO remove, this demo shouldn't need to reset the theme.
 
 export default function Report() {
+    const [value, setValue] = useState('');
 
     return (
-        <Container component="main">
-            <Box
-                sx={{
-                    mt: 8,
-                    display: 'flex',
-                    flexDirection: 'column',
-                    alignItems: 'center',
-                    mb: 8
-                }}
-            >
-                <Paper elevation={5} sx={{ p: 5 }}>
-
-                    Report
-                </Paper>
+        <Box
+            sx={{
+                mt: 8,
+                display: 'flex',
+                mb: 8
+            }}
+        >
 
 
-            </Box>
-        </Container>
+            <Grid container>
+
+
+                <Grid item xs={8}>
+                    <Paper elevation={5} sx={{ p: 5 }}>
+
+                        <ReactQuill theme="snow" value={value} onChange={setValue} />
+                        <Card sx={{ maxWidth: 345 }}>
+                            <CardMedia
+                                component="img"
+                                alt="green iguana"
+                                height="140"
+                                image="/static/images/cards/contemplative-reptile.jpg"
+                            />
+                            <CardContent>
+                                <Typography gutterBottom variant="h6" component="div">
+                                    Lizard
+                                </Typography>
+                            </CardContent>
+                        </Card>
+                    </Paper>
+                </Grid>
+
+                <Grid item xs={4}>
+                    <Paper>
+                        <Stack direction="column"
+                            sx={{
+                                padding: '30px',
+                                marginTop: 8,
+                            }} component="form">
+                            <Grid container spacing={2}>
+                                <Grid item xs={12}>
+                                    <Typography sx={{ fontWeight: 'bold' }}>Apellidos</Typography>
+                                    <TextField
+                                        fullWidth
+                                        sx={{ minWidth: "400px" }}
+                                        margin="normal"
+                                        required
+                                        name="email"
+                                        autoComplete="email"
+                                        autoFocusrequired
+                                        id="outlined-required"
+                                        label="Required"
+                                        defaultValue="Hello World"
+                                    />
+                                </Grid>
+
+                                <Grid item xs={12}>
+                                    <Typography sx={{ fontWeight: 'bold' }}>correo</Typography>
+                                    <TextField
+                                        fullWidth
+                                        margin="normal"
+                                        required
+                                        name="email"
+                                        autoComplete="email"
+                                        autoFocusrequired
+                                        id="outlined-required"
+                                        label="Required"
+                                        defaultValue="Hello World"
+                                    />
+                                </Grid>
+
+
+                                <Grid item xs={12}>
+                                    <Typography sx={{ fontWeight: 'bold' }}>Telefono</Typography>
+                                    <TextField
+                                        fullWidth
+                                        margin="normal"
+                                        required
+                                        name="email"
+                                        autoComplete="email"
+                                        autoFocusrequired
+                                        id="outlined-required"
+                                        label="Required"
+                                        defaultValue="Hello World"
+                                    />
+                                </Grid>
+
+
+                                <Grid item xs={12} >
+                                    <Typography sx={{ fontWeight: 'bold' }}>Tipo de Sangre:</Typography>
+                                    <TextField
+                                        fullWidth
+                                        margin="normal"
+                                        required
+                                        name="email"
+                                        autoComplete="email"
+                                        autoFocusrequired
+                                        id="outlined-required"
+                                        label="Required"
+                                        defaultValue="Hello World"
+                                    />
+                                </Grid>
+                            </Grid>
+                        </Stack>
+                    </Paper>
+
+                </Grid>
+
+            </Grid>
+
+
+
+        </Box>
     );
 }
