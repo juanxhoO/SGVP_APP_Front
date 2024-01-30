6a591df57fb00bc8df966d032f2fd751fc1d107d
juan <juanxhoo@gmail.com>
2024-01-29 20:26:19 -0500

    data send

--- src/layouts/dashboard/Dashboard.js
+++ src/layouts/dashboard/Dashboard.js
@@ -3,14 +3,10 @@ import { styled, createTheme } from '@mui/material/styles';
 import Box from '@mui/material/Box';
 import { VehicleDash } from './VehicleDash';
 import { PieDash } from './PieDash';
-import { RadarDash } from './RadarDash';
 import { AreaDash } from './AreaDash';
 import { LineDash } from './LineDash';
 import { DonutDash } from './DonutDash';
-import { Grid, Paper } from '@mui/material';
-
-
-// TODO remove, this demo shouldn't need to reset the theme.
+import { Grid, Paper, Typography } from '@mui/material';
 
 export default function MainDashboard() {
   return (
@@ -18,14 +14,14 @@ export default function MainDashboard() {
       <Grid container sx={{ display: 'flex', alignItems: 'center', p: 4 }}>
         <Grid item xs={4}>
           <Paper>
+            sdsds
+            <Typography>Mantenimientos</Typography>
             <PieDash />
-
           </Paper>
         </Grid>
         <Grid item xs={4}>
           <Paper>
             <VehicleDash />
-
           </Paper>
         </Grid>
         <Grid item xs={4}>
